a2980af5b8e873be5b1a2dc325daa1e8db25f6f8
trieucd <trieucd@runsystem.net>
2023-08-31 15:35:52 +0700

    chore: update desc

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -31,7 +31,7 @@ export default function Home(): JSX.Element {
   return (
     <Layout
       title={`Homepage`}
-      description="Description will go into a meta tag in <head />">
+      description="Documa">
       <HomepageHeader />
       <main>
         <HomepageFeatures />
